b55b4ac603d06656947d22b7a571043ab14b49f7
Andrew Eckes <aeckes@nvidia.com>
2019-12-16 13:06:10 -0800

    level-1 working

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,62 @@
 // from data.js
 var tableData = data;
+var columns = ['datetime','city','state','country','shape','durationMinutes','comments']
 
-// YOUR CODE HERE!
+// select button
+var button = d3.select("#filter-btn");
+
+// event of click
+
+button.on('click', function() {
+
+    // get date input from form
+    var dateForm = d3.select('#datetime');
+    var userDate = dateForm.property('value');
+
+    // filter using date
+    var filteredData = data.filter(obs => obs.datetime === userDate)
+
+    // check for proper filtering in console
+    console.log(filteredData);
+
+    // create table oof filtered results
+    createTable(filteredData, columns);
+
+});
+
+// create table
+function createTable(data, columns) {
+
+    // select table body - headers are static
+    var table = d3.select('tbody');
+
+    // clear any existing results
+    table.html("");
+
+    // create table rows based on length of filtered data
+    var rows = table.selectAll('tr')
+        .data(data)
+        .enter()
+        .append('tr');
+
+    // create table cells, use column names to get each value - I borrowed this :/
+    var cells = rows.selectAll('td')
+        .data(function(row) {
+            return columns.map(function(column) {
+                    return {column: column, value: row[column]};
+            });
+        })
+        .enter()
+        .append('td')
+        .html(function(d) {return d.value}); //add data to each cell
+
+    return table;
+
+}
+
+
+
+
+
+
+// list.append('li').text(`text ${var}`)
